5e3f900719e9463d25c927e70bb2c26d510c99d6
Whinnes <whinnes@gmail.com>
2020-10-08 00:11:46 -0400

    Created function to read json file

--- src/app.js
+++ src/app.js
@@ -1,25 +1,23 @@
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
-const bodyParser = require('body-parser')
 
 //Init
 const app = express()
 
 //Settings
-app.set(bodyParser.urlencoded({extended: false}))
-app.set(bodyParser.json())
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', '.ejs')
+app.set('view engine', 'ejs')
+
 
 //Middlewares
 app.use(morgan('dev'))
-
-
-//Static Files
-app.set('public', path.join(__dirname, 'public'))
+app.use(express.urlencoded({extended: false}))
 
 //Routes
 app.use(require('./routes/tasks.routes'))
+
+//Static Files
+app.use(express.static(path.join(__dirname, 'public')))
 
 module.exports = app
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,23 +1,34 @@
 const router = require('express').Router()
 const fs = require('fs')
 
-let tasks = []
 
-router.get('/',(req,res)=>{
-    res.render('index')
+const json_tasks = fs.readFileSync('src/tasks.json','utf-8')
+const tasks = JSON.parse(json_tasks)
+
+router.get('/', (req, res) => {
+    res.render('index', {tasks})
 })
 
-router.get('/add',(req,res)=>{
+router.get('/add', (req, res) => {
     res.render('addtask')
 })
 
-router.post('/add',(req,res)=>{
+router.post('/add', (req, res) => {
+    const {title, description} = req.body
+    let newTask = {
+        title,
+        description
+    }
+    tasks.push(newTask)
+    const json_tasks = JSON.stringify(tasks)
+    fs.writeFileSync('src/tasks.json', json_tasks, 'utf-8')
     console.log(req.body)
-    res.send('Task received')
-
+    res.render('index.ejs', {
+        tasks
+    })
 })
 
-router.get('/delete',(req,res)=>{
+router.get('/delete', (req, res) => {
     res.send('Delete task')
 })
 

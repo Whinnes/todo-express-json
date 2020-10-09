95766201775cfa5e897427424734dc8672c6f65c
Whinnes <whinnes@gmail.com>
2020-10-08 23:01:06 -0400

    Created edit and delete function

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const fs = require('fs')
+const {v4:uuidv4} = require('uuid')
 
 
 const json_tasks = fs.readFileSync('src/tasks.json','utf-8')
-const tasks = JSON.parse(json_tasks)
+let tasks = JSON.parse(json_tasks)
 
 router.get('/', (req, res) => {
     res.render('index', {tasks})
@@ -16,6 +17,7 @@ router.get('/add', (req, res) => {
 router.post('/add', (req, res) => {
     const {title, description} = req.body
     let newTask = {
+        id: uuidv4(),
         title,
         description
     }
@@ -28,8 +30,43 @@ router.post('/add', (req, res) => {
     })
 })
 
-router.get('/delete', (req, res) => {
-    res.send('Delete task')
+router.get('/delete/:id', (req, res) => {
+    tasks = tasks.filter(task => task.id != req.params.id)
+    const json_tasks = JSON.stringify(tasks)
+    fs.writeFileSync('src/tasks.json', json_tasks, 'utf-8')
+    res.redirect('/')
 })
+
+router.get('/edit/:id', (req, res) => {
+    const myTask = tasks.filter(task => task.id == req.params.id)
+    let myTaskab = {
+        id: myTask[0].id,
+        title: myTask[0].title,
+        description: myTask[0].description
+    }
+    res.render('edittask.ejs', {myTaskab})
+})
+
+router.post('/edit/:id', (req, res) => {
+    const {id, title, description} = req.body
+    let newTask = {
+        id,
+        title,
+        description
+    }
+
+    tasks = tasks.filter(task => task.id != req.params.id)
+    const json_tasks = JSON.stringify(tasks)
+    fs.writeFileSync('src/tasks.json', json_tasks, 'utf-8')
+
+    tasks.push(newTask)
+    const json_tasksA = JSON.stringify(tasks)
+    fs.writeFileSync('src/tasks.json', json_tasksA, 'utf-8')
+    console.log(req.body)
+    res.render('index.ejs', {
+        tasks
+    })
+})
+
 
 module.exports = router
